996afbd848e08cfbd72c329d5cb6663b9e2650ff
zjc <928304668@qq.com>
2024-03-21 16:18:40 +0800

    PT文件分割

---
+++ PT_cut_test.py
@@ -0,0 +1,17 @@
+import re
+
+
+def pt_read(text):
+    with open(text, 'r', encoding='utf-8') as file:
+        txt_content = file.read()
+
+    # 使用splitlines()并结合列表推导式，将连续的非空行视为一个段落
+    paragraphs = [''.join(paragraph.splitlines()) for paragraph in txt_content.split('\n\n') if paragraph.strip()]
+
+    return paragraphs
+
+
+if __name__ == '__main__':
+    paragraphs = pt_read('D:/Documents/1.txt')
+    for paragraph in paragraphs:
+        print(paragraph)
